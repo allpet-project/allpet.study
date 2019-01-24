096db37a676bb2f58be11dcec4f948f68f2a29de
dongbo <895188445@qq.com>
2019-01-24 19:29:21 +0800

    start

--- blockChain/akka.study/Program.cs
+++ blockChain/akka.study/Program.cs
@@ -7,12 +7,60 @@ namespace akka.study
     {
         static void Main(string[] args)
         {
-            using (var system = ActorSystem.Create("iot-system"))
+           var MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+            var helloactor = MyActorSystem.ActorOf(Props.Create(()=>new Helloactor()));
+            helloactor.Tell("");
+
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() =>
+                new ConsoleWriterActor()));
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() =>
+                new ConsoleReaderActor(consoleWriterActor)));
+
+            consoleReaderActor.Tell("start");
+
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+
+    internal class ConsoleReaderActor : UntypedActor
+    {
+        private IActorRef consoleWriterActor;
+
+        public ConsoleReaderActor(IActorRef consoleWriterActor)
+        {
+            this.consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var read = Console.ReadLine();
+            consoleWriterActor.Tell(read);
+            Self.Tell("continue");
+        }
+    }
+
+    internal class ConsoleWriterActor:UntypedActor
+    {
+        public ConsoleWriterActor()
+        {
+
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
             {
-                // Create top level supervisor
-                var supervisor = system.ActorOf(Props.Create<IotSupervisor>(), "iot-supervisor");
-                // Exit the system after ENTER is pressed
-                Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Please provide an input.\n");
+                Console.ResetColor();
+                return;
+            }else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("typed:"+msg);
+                Console.ResetColor();
             }
         }
     }
---
+++ blockChain/akka.study/helloactor.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace akka.study
+{
+    class Helloactor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine("hello akka.net!");
+        }
+    }
+}
