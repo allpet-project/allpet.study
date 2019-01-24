cb171e6c24214939d6193424977573c26a4cbd29
dongbo <895188445@qq.com>
2019-01-24 12:39:46 +0800

    block chain study

---
+++ blockChain/blockChain/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Timers;
+
+namespace blockChain
+{
+    class Program
+    {
+        static Timer timer;
+        static void Main(string[] args)
+        {
+            timer = initTimer();
+            timer.Elapsed += produceBlock;
+
+            while (true) { };
+        }
+        static Timer initTimer()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 3000;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            return timer;
+        }
+
+        static void produceBlock(object sender, ElapsedEventArgs e)
+        {
+            var time = DateTime.Now;
+            Console.WriteLine("produce block. time:"+time.ToString());
+        }
+
+
+    }
+}
