048fbf22f8226fa92258598741f63d5be494db9d
dongbo <895188445@qq.com>
2019-01-24 18:18:52 +0800

    akka.net learn start

---
+++ blockChain/akka.study/Program.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+
+namespace akka.study
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var system = ActorSystem.Create("iot-system"))
+            {
+                // Create top level supervisor
+                var supervisor = system.ActorOf(Props.Create<IotSupervisor>(), "iot-supervisor");
+                // Exit the system after ENTER is pressed
+                Console.ReadLine();
+            }
+        }
+    }
+}
